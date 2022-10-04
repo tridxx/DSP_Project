dbe23bc9370d2f0c3a3ff12cd04b21af0d84cdf8
86150 <2596767167@qq.com>
2022-10-04 15:21:57 +0800

    修改了一点

--- __main__.py
+++ __main__.py
@@ -2,17 +2,25 @@
 import numpy as np
 from scipy.fft import fft
 
+
 # 想法：自己写一个spec_x=myfft(x)，还没写[5]DCT和h_mel，别的写完了
+def mcff(spec_x, num_filter, n):  # s是Mel滤波器能量,n是阶数，num_filter是Mel滤波器数量,求某一帧s的DCT得到的mcff系数
+    res = 0
+    for i in range(num_filter):
+        res = res + np.log(spec_x[num_filter]) * np.cos(np.pi * n * (2 * num_filter - 1) / (2 * num_filter))
+    return np.sqrt(2 / num_filter) * res
+
+
+def energy_cal(spec_x, num_filter, h_mel):  # spec_x是一帧的信号的频谱
+    spec_energy = np.power(np.real(spec_x), 2) + np.power(np.imag(spec_x), 2)
+    spec_mel = np.zeros(num_filter)
+    one = np.ones(len(spec_x))
+    for m in range(num_filter):
+        spec_mel[m] = np.multiply(spec_energy, h_mel[m]) * one  # 相乘后求和
+
 
 t = np.linspace(0, 5 * np.pi, 200)  # 时间坐标
 x = np.sin(2 * np.pi * t)  # 正弦函数
-
-spec_x = fft(x)
-spec_energy = np.power(np.real(spec_x), 2) + np.power(np.imag(spec_x), 2)
-num_filter = 10
-spec_mel = np.zeros(num_filter)
-h_mel = np.zeros(num_filter)  # Mel滤波器的频率响应
-one = np.ones(len(spec_energy))
-print(spec_mel)
-for m in range(len(spec_energy)):
-    spec_mel[m] = np.multiply(spec_energy, h_mel[m]) * one  # 相乘后求和
+# 下面写的都是第i帧的，需要再修改
+s = fft(x)
+num = 10
